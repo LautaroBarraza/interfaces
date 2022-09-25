d5a59781da86a3aad8faacee9cbe038e250bd894
Doroteo Vidal <doroteo17vidal@hotmail.com>
2022-09-25 19:26:34 -0300

    menu desplegable y footer responsive

--- entrega2/js/script.js
+++ entrega2/js/script.js
@@ -1,7 +1,9 @@
 'use strict'
 
-document.querySelector('.icono-menu').addEventListener('click', toggleMenu);
+document.querySelector('.icon-main').addEventListener('click', () =>{
+    document.querySelector(".nav").classList.toggle("show");
+});
+document.querySelector('.icon-user').addEventListener('click', () =>{
+    document.querySelector(".user-nav").classList.toggle("showUser");
+});
 
-function toggleMenu() {
-    document.querySelector("nav").classList.toggle("show");
-}
