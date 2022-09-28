a5b62b77fcd05ace82c6e0cbec86e02b716c0885
Lautaro Barraza <lau.barraza1206@gmail.com>
2022-09-28 18:57:21 -0300

    carrusel animado

---
+++ entrega2/js/scriptCarrusel.js
@@ -0,0 +1,24 @@
+"use strict"
+
+document.addEventListener("DOMContentLoaded", cargar);
+
+function cargar(){
+    const carruseles = [...document.querySelectorAll(".juego-carrusel")];
+    console.log(carruseles);
+    const nxtBtn = [...document.querySelectorAll(".categoria-boton-derecha")];
+    const preBtn = [...document.querySelectorAll(".categoria-boton-izquierda")];
+
+    carruseles.forEach((item,i)=> {
+        let dimensionContenedor = item.getBoundingClientRect();
+        let contenedorWidth = dimensionContenedor.width;
+
+        nxtBtn[i].addEventListener("click", () => {
+            item.scrollLeft += contenedorWidth;
+        })
+
+        preBtn[i].addEventListener("click", () => {
+            item.scrollLeft -= contenedorWidth;
+        })
+
+    })
+}
