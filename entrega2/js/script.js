4037cb81514a96acbadd654389b1db50a066fd5f
Doroteo Vidal <doroteo17vidal@hotmail.com>
2022-09-29 11:31:25 -0300

    Se agrega preloader

--- entrega2/js/script.js
+++ entrega2/js/script.js
@@ -1,29 +1,47 @@
 'use strict'
+document.addEventListener("DOMContentLoaded", function (){
 
-let main = document.querySelector(".nav");
-let user = document.querySelector(".user-nav");
+    //Preloader//
+    let numero = document.querySelector('#porcentaje');
+    let contador = 0;
+    let loader = document.querySelector('#onload');
+    setInterval(()=>{
+        if(contador==100){
+            clearInterval();
+            loader.remove(loader);
+            document.querySelector('.hidden').style.overflow = "visible";
+            document.querySelector('.hidden').style.animation = "fadeOut 1.3s";
+        }else{
+            contador+=1;
+            numero.textContent = contador + "%";
+        }
+    }, 20);
+    
+    //desplegables//
+    let main = document.querySelector(".nav");
+    let user = document.querySelector(".user-nav");
 
-document.querySelector('.icon-main').addEventListener('click', () =>{
-    if(user.classList.contains('showUser')){
-        user.classList.remove('showUser');
+    document.querySelector('.icon-main').addEventListener('click', () =>{
+        if(user.classList.contains('showUser')){
+            user.classList.remove('showUser');
+        }
+        main.classList.toggle("show");
+        document.querySelector(".dark-background").classList.add("on");
+        comprobar();
+    });
+
+    document.querySelector('.icon-user').addEventListener('click', () =>{
+        if(main.classList.contains('show')){
+            main.classList.remove('show');
+        }
+        user.classList.toggle("showUser");
+        document.querySelector(".dark-background").classList.add("on");
+        comprobar();
+    });
+
+    function comprobar(){
+        if(!main.classList.contains('show') && !user.classList.contains('showUser')){
+            document.querySelector(".dark-background").classList.remove("on");
+        }
     }
-    main.classList.toggle("show");
-    document.querySelector(".dark-background").classList.add("on");
-    comprobar();
 });
-
-document.querySelector('.icon-user').addEventListener('click', () =>{
-    if(main.classList.contains('show')){
-        main.classList.remove('show');
-    }
-    user.classList.toggle("showUser");
-    document.querySelector(".dark-background").classList.add("on");
-    comprobar();
-});
-
-function comprobar(){
-    if(!main.classList.contains('show') && !user.classList.contains('showUser')){
-        document.querySelector(".dark-background").classList.remove("on");
-    }
-}
-
