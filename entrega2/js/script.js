04e98200620e58d8b76c9635206c0c0920f9168d
Doroteo Vidal <doroteo17vidal@hotmail.com>
2022-09-28 22:53:08 -0300

    se agrega funcionalidad en botones del menu

--- entrega2/js/script.js
+++ entrega2/js/script.js
@@ -1,10 +1,29 @@
 'use strict'
 
+let main = document.querySelector(".nav");
+let user = document.querySelector(".user-nav");
+
 document.querySelector('.icon-main').addEventListener('click', () =>{
-    document.querySelector(".nav").classList.toggle("show");
-});
-document.querySelector('.icon-user').addEventListener('click', () =>{
-    document.querySelector(".user-nav").classList.toggle("showUser");
+    if(user.classList.contains('showUser')){
+        user.classList.remove('showUser');
+    }
+    main.classList.toggle("show");
+    document.querySelector(".dark-background").classList.add("on");
+    comprobar();
 });
 
+document.querySelector('.icon-user').addEventListener('click', () =>{
+    if(main.classList.contains('show')){
+        main.classList.remove('show');
+    }
+    user.classList.toggle("showUser");
+    document.querySelector(".dark-background").classList.add("on");
+    comprobar();
+});
+
+function comprobar(){
+    if(!main.classList.contains('show') && !user.classList.contains('showUser')){
+        document.querySelector(".dark-background").classList.remove("on");
+    }
+}
 
