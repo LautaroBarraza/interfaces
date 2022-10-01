f66acad4b121e824b73dc876933e9b6457ba9878
lautaro barraza <lau.barraza1206@gmail.com>
2022-09-30 23:00:04 -0300

    paginacion carrusel

--- entrega2/js/scriptCarrusel.js
+++ entrega2/js/scriptCarrusel.js
@@ -7,6 +7,7 @@ function cargar(){
     console.log(carruseles);
     const nxtBtn = [...document.querySelectorAll(".categoria-boton-derecha")];
     const preBtn = [...document.querySelectorAll(".categoria-boton-izquierda")];
+    const NAV_CARRUSEL = [...document.querySelectorAll(".pag-carrusel")];
 
     carruseles.forEach((item,i)=> {
         let dimensionContenedor = item.getBoundingClientRect();
@@ -22,6 +23,24 @@ function cargar(){
         preBtn[i].addEventListener("click", () => {
             item.scrollLeft -= contenedorWidth;
         })
+
+        let cantidad_nav=Math.trunc( item.scrollWidth/contenedorWidth);
+
+        for(let j=-1;j<cantidad_nav;j++){
+            let button = document.createElement("button");
+            if(j===-1){
+                button.classList.add("activo")
+            }
+            NAV_CARRUSEL[i].appendChild(button);
+            button.addEventListener("click",(e) => {
+                item.scrollLeft = contenedorWidth * (j+1);
+                NAV_CARRUSEL[i].querySelector(".activo").classList.remove("activo");
+                e.target.classList.add("activo");
+            })
+            
+        }
+
+
 
     })
 }
