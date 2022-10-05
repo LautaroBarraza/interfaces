5ac9f1276f7fc2586c8c242af772a44dc53a017c
Lautaro Barraza <lau.barraza1206@gmail.com>
2022-10-05 19:06:30 -0300

    funcionalidad flecha hero

--- entrega2/js/scriptHero.js
+++ entrega2/js/scriptHero.js
@@ -5,13 +5,21 @@ document.addEventListener("DOMContentLoaded", cargar);
 function cargar(){
 
     let header = document.querySelector("header");
+    let hero_tamanio = document.querySelector(".hero").clientHeight;
     window.addEventListener("scroll", function(){
-        let hero_tamanio = this.document.querySelector(".hero").clientHeight;
         if(this.window.scrollY>= hero_tamanio){
             header.classList.remove("header-hero");
         }else{
             header.classList.add("header-hero");
         }
+    })
+
+    document.querySelector(".arrow-hero").addEventListener("click", () => {
+        window.scroll({
+            top: hero_tamanio,
+            left: 0,
+            behavior: 'smooth'
+          });
     })
     
 
