8997979e1728ae17e38b3e589dbddf89c4e11946
Doroteo Vidal <doroteo17vidal@hotmail.com>
2022-11-12 12:02:48 -0300

    punto 6 realizado

--- entrega4/js/scriptHero.js
+++ entrega4/js/scriptHero.js
@@ -6,17 +6,17 @@ function load(){
 
     //seccion hero//
 
-    let prof1= document.querySelector(".prof1");
-    let prof2= document.querySelector(".prof2");
-    let prof3= document.querySelector(".prof3");
+    let prof1 = document.querySelector(".prof1");
+    let prof2 = document.querySelector(".prof2");
+    let prof3 = document.querySelector(".prof3");
 
     window.addEventListener("scroll", moveHero);
 
     function moveHero(){
-        let value= window.scrollY;
-        prof1.style.top= value*0.5+"px";
-        prof2.style.bottom=-1+"px";
-        prof3.style.top=value+50*0.8+"px";
+        let value = window.scrollY;
+        prof1.style.top = value * 0.5 + "px";
+        prof2.style.bottom = -1 + "px";
+        prof3.style.top = value + 50 * 0.8 + "px";
         console.log(value)
     }
 
@@ -24,7 +24,7 @@ function load(){
     let header = document.querySelector("header");
     let hero_tamanio = document.querySelector(".hero").clientHeight;
     window.addEventListener("scroll", function(){
-        if(this.window.scrollY>= hero_tamanio){
+        if(this.window.scrollY >= hero_tamanio){
             header.classList.remove("header-hero");
         }else{
             header.classList.add("header-hero");
@@ -40,5 +40,27 @@ function load(){
           });
     })
     
+    //Scroll cards hero
+    let faders = document.querySelectorAll('.fade-in');
+    let sliders = document.querySelectorAll('.slide-in');
+
+    const appearOnScroll = new IntersectionObserver(function(entries, appearOnScroll){
+        entries.forEach(entry => {
+            if(!entry.isIntersecting){
+                return;
+            }else{
+                entry.target.classList.add('appear');
+                appearOnScroll.unobserve(entry.target);
+            }
+        })
+    });
+
+    faders.forEach(fader =>{
+        appearOnScroll.observe(fader);
+    })
+
+    sliders.forEach(slider =>{
+        appearOnScroll.observe(slider);
+    })
 
 }
